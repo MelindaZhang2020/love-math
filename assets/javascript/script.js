770758c041a54e6271bd4604cd0cd4347f32b6a9
MelindaZhang2020 <melinda_hongye@yahoo.com>
2021-02-16 09:43:43 +0000

    added division game, finished challenge!

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -131,9 +131,13 @@ function displayMultiplyQuestion(operand1,operand2) {
 }
 function displayDivisionQuestion(operand1,operand2) {
 
-    operand1 % operand2 === 0;
+    
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "/";
-
+    if (operand1 % operand2 === 0) {
+        return operand1, operand2;
+    } else {
+        runGame ("division");
+    }
 }
